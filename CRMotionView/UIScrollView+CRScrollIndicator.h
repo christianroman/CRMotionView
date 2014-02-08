dcad7bf18d51a4d7748bbd4bf31910f139333b25
Christian Roman <chroman16@gmail.com>
2014-02-08 15:52:26 -0600

    UIScrollView scroll indicator added

---
+++ CRMotionView/UIScrollView+CRScrollIndicator.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+CRScrollIndicator.h
+//  CRMotionViewDemo
+//
+//  Created by Christian Roman on 07/02/14.
+//  Copyright (c) 2014 Christian Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (CRScrollIndicator)
+
+@end
---
+++ CRMotionView/UIScrollView+CRScrollIndicator.m
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+CRScrollIndicator.m
+//  CRMotionViewDemo
+//
+//  Created by Christian Roman on 07/02/14.
+//  Copyright (c) 2014 Christian Roman. All rights reserved.
+//
+
+#import "UIScrollView+CRScrollIndicator.h"
+
+@implementation UIScrollView (CRScrollIndicator)
+
+@end
